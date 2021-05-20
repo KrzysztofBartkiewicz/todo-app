e8b1bd60726eb87292181984863a147d66836fc4
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-05-20 14:33:26 +0200

    Install fontawesome

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,13 @@
+import './App.scss';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className="app__heading">Lista zadań</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
