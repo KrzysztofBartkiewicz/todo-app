2731b6e6cb77820a5cfcfedbfc9c9eccfcdc8ad9
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-05-21 11:34:01 +0200

    Add text area atom copmonent

---
+++ src/components/atoms/TextArea/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledTextArea } from './StyledTextArea';
+
+const TextArea = ({ value }) => {
+  return <StyledTextArea value={value} />;
+};
+
+export default TextArea;
